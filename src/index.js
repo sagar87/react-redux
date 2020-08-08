105a06333bccfe5a9a618e718cb0904ab6ceea19
Harald Sagar Vöhringer <harald@Haralds-MacBook-Pro.local>
2020-08-08 11:46:03 +0100

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import store from "./customStore";
+import store from "./store";
 
 import * as actions from "./actions";
 
 store.subscribe(() => console.log("store changed"));
 
-store.dispatch(actions.bugAdded("bugAdded 1"));
+store.dispatch(actions.bugAdded("But 1"));
+store.dispatch(actions.bugAdded("But 5"));
+store.dispatch(actions.bugAdded("But 2"));
+store.dispatch(actions.bugAdded("But 3"));
+store.dispatch(actions.bugAdded("But 4"));
 console.log(store.getState());
