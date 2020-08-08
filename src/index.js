317e7633c46591c7b9802b6657c9416dd5d783ea
Harald Sagar Vöhringer <harald@Haralds-MacBook-Pro.local>
2020-08-08 11:10:01 +0100

    added actionTypes.js, replaced hardcoded actiontypes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import store from "./store";
+import * as actions from "./actionTypes";
 
 // subscribe to the store
 // subscribe yields an unsubscribe function
@@ -11,7 +12,7 @@ console.log(store);
 console.log(store.getState());
 
 store.dispatch({
-  type: "bugAdded",
+  type: actions.BUG_ADDED,
   payload: { description: "Bug1" },
 });
 
@@ -20,7 +21,7 @@ console.log(store.getState());
 unsubscribe();
 
 store.dispatch({
-  type: "bugRemoved",
+  type: actions.BUG_REMOVED,
   payload: { id: 1 },
 });
 
