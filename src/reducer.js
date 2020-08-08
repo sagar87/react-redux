ddd47f0edf5958e8409bcac8a8f47817278a021d
Harald Sagar Vöhringer <harald@Haralds-MacBook-Pro.local>
2020-08-08 10:58:47 +0100

    refactored reducer to switch and case

--- src/reducer.js
+++ src/reducer.js
@@ -1,21 +1,23 @@
 let lastId = 0;
 function reducer(state = [], action) {
-  if (action.type === "bugAdded")
-    return [
-      ...state,
-      {
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-  else if (action.type === "bugRemoved") {
-    return state.filter((bug) => bug.id !== action.payload.id);
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
+    default:
+      return state;
   }
-  return state;
 }
 
 // Notes:
 // action.payload should contain the minum information to update the reducer
 // everything else should be computed in the reducer
-//
+// A reducer is a pure function
